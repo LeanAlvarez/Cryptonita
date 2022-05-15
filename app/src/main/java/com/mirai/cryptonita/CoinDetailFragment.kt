3c0d2b6dd76b0cb418199ea0ee692eb7e4d188da
leanalvarez <leanalvarez.dev@gmail.com>
2022-05-14 21:09:21 -0300

    primer commit

---
+++ app/src/main/java/com/mirai/cryptonita/CoinDetailFragment.kt
@@ -0,0 +1,15 @@
+package com.mirai.cryptonita
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class CoinDetailFragment : Fragment(R.layout.fragment_coin_detail) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+}
